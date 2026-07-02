1352aafa9979ae123e7b116ddaba4f9b43622d6a
GuilhermeMen <gui.mcastro11@gmail.com>
2026-07-01 21:53:26 -0400

    Delete models directory

--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .item_biblioteca import ItemBiblioteca
-from .livro import Livro
-from .revista import Revista
-from .usuario import Usuario
-from .emprestimo import Emprestimo
--- models/usuario.py
+++
@@ -1,8 +0,0 @@
-class Usuario:
-
-    def __init__(self, nome, id_usuario):
-        self.nome = nome
-        self.id_usuario = id_usuario
-
-    def __str__(self):
-        return f"{self.nome} ({self.id_usuario})"
